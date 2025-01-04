458f916f3175503e4b57993f04c6ac8d3c2af96c
Alphius <alphiuswambua@gmail.com>
2025-01-04 16:14:25 +0300

    completed Crud for leads

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,16 @@
-from .views import LeadCreateView, LeadListView
+from .views import (
+    LeadCreateView, LeadListView, 
+    LeadRetrieveView, LeadUpdateView, 
+    LeadDestroyView
+)
 from django.urls import path
 
 app_name = 'leads'
 
 urlpatterns = [
-     path('create/', LeadCreateView.as_view(), name='leads-create'),
-     path('list/', LeadListView.as_view(), name='leads-list'),
+    path('list/', LeadListView.as_view(), name='lead-list'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
+    path('<int:pk>/', LeadRetrieveView.as_view(), name='lead-retrieve'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
+    path('<int:pk>/delete/', LeadDestroyView.as_view(), name='lead-delete'),
 ]
--- leads/views.py
+++ leads/views.py
@@ -5,14 +5,28 @@ from rest_framework.generics import (
     ListAPIView, RetrieveAPIView, CreateAPIView,
     UpdateAPIView, DestroyAPIView
 )
-from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.permissions import AllowAny
 
 class LeadCreateView(CreateAPIView):
-    permission_classes = (AllowAny, )
+    permission_classes = (AllowAny,)
     serializer_class = LeadSerializer
     queryset = Lead.objects.all()
 
 class LeadListView(ListAPIView):
     permission_classes = (AllowAny,)
     serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
+
+class LeadRetrieveView(RetrieveAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
+
+class LeadUpdateView(UpdateAPIView):
+    permission_classes = (AllowAny,)
+    serializer_class = LeadSerializer
+    queryset = Lead.objects.all()
+
+class LeadDestroyView(DestroyAPIView):
+    permission_classes = (AllowAny,)
     queryset = Lead.objects.all()
